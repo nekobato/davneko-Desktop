6c142c2c57c5485bc03c6119030aef917a5abfd9
nekobato <nekobato@gmail.com>
2016-11-25 18:01:21 +0900

    update build module & script

--- build.js
+++ build.js
@@ -1,6 +1,4 @@
 path = require('path');
-gulp = require('gulp');
-util = require('gulp-util');
 packager = require('electron-packager');
 appPackage = require('./package.json');
 
@@ -9,7 +7,7 @@ const buildOption = {
   name: appPackage.name,
   platform: 'darwin', // linux, win32, darwin, all
   arch: 'x64', // ia32, x64, all
-  version: process.env.ELECTRON || '1.1.1',
+  version: process.env.ELECTRON || '1.1.2',
   'app-bundle-id': `${appPackage.author}.${appPackage.name}`,
   'app-version': appPackage.version,
   'build-version': appPackage.version,
@@ -20,14 +18,13 @@ const buildOption = {
   out: './release'
 }
 
-gulp.task('release', () => {
-  var start = Date.now();
 
-  packager(buildOption, (err, appPath) => {
-    if (err) console.log('[ERROR]', err);
-    console.log('[appPath]', appPath);
-    var end = Date.now();
+const start = Date.now()
 
-    console.log('[relase]', `${end  - start} MilliSeconds.`);
-  });
-});
+packager(buildOption, (err, appPath) => {
+  if (err) console.log('[ERROR]', err)
+  console.log('[appPath]', appPath)
+  var end = Date.now()
+
+  console.log('[relase]', `${end  - start} MilliSeconds.`)
+})
